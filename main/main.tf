6819546980e31c7e6c76337c015a0c64cfb664bf
jueun0819 <98609303+jueun0819@users.noreply.github.com>
2022-02-04 21:29:15 -0600

    Update main.tf

--- main/main.tf
+++ main/main.tf
@@ -1,50 +1,21 @@
-##################################################################################
-# LOCALS
-##################################################################################
-
-
-locals {
-  resource_group_name = "${var.naming_prefix}-${random_integer.name_suffix.result}"
-  app_service_plan_name = "${var.naming_prefix}-${random_integer.name_suffix.result}"
-  app_service_name = "${var.naming_prefix}-${random_integer.name_suffix.result}"
-}
-
-resource "random_integer" "name_suffix" {
-  min = 10000
-  max = 99999
-}
-
-##################################################################################
-# APP SERVICE
-##################################################################################
-
-resource "azurerm_resource_group" "app_service" {
-  name     = local.resource_group_name
-  location = var.location
-}
-
-resource "azurerm_app_service_plan" "app_service" {
-  name                = local.app_service_plan_name
-  location            = azurerm_resource_group.app_service.location
-  resource_group_name = azurerm_resource_group.app_service.name
-
-  sku {
-    tier = var.asp_tier
-    size = var.asp_size
-    capacity = var.capacity
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.91.0"
+    }
   }
 }
 
-resource "azurerm_app_service" "app_service" {
-  name                = local.app_service_name
-  location            = azurerm_resource_group.app_service.location
-  resource_group_name = azurerm_resource_group.app_service.name
-  app_service_plan_id = azurerm_app_service_plan.app_service.id
-  
-  source_control {
-    repo_url = "https://github.com/ned1313/nodejs-docs-hello-world"
-    branch = "main"
-    manual_integration = true
-    use_mercurial = false
-  }
+provider "azurerm" {
+  version = ">=2.0"
+  skip_provider_registration = "true"
+  # The "feature" block is required for AzureRM provider 2.x.
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources2"
+  location = "West Europe"
 }
